8c4f50708db5618b85b0c6e3765e01b8216a899f
Anth040620 <anthonybelenbatista1@gmail.com>
2024-07-29 15:34:35 -0400

    first commit

--- footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-    <p>&copy; <?php echo date('Y'); ?> Mi Librería. Todos los derechos reservados.</p>
-</footer>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<script src="js\scripts.js"></script>
-</body>
-</html>
