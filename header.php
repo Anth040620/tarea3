2da55bf4074661d186389a7b1f41b299e1b323b4
Anth040620 <anthonybelenbatista1@gmail.com>
2024-07-29 15:25:11 -0400

    first commit

---
+++ header.php
@@ -0,0 +1,25 @@
+    <!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Librería Online</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <button id="menu-toggle" class="menu-toggle">☰</button>
+        <nav id="nav-menu" class="nav-menu hidden">
+            <ul>
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="libros.php">Libros</a></li>
+                <li><a href="autores.php">Autores</a></li>
+                <li><a href="contacto.php">Contacto</a></li>
+            </ul>
+        </nav>
+    </header>
+
+
+
+
